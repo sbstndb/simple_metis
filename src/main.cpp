c0f5c77388067214df06a433dfc010c91e93e4d7
sbstndbs <sebastiendubois4@gmail.com>
2025-02-25 10:30:26 +0100

    Add doc for strategies

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,18 @@ int main() {
 	// scotch datastructure
 	SCOTCH_Graph graphExemple;
 
-
-
 	// Basic usage of scotch
 	SCOTCH_Strat strat ; 
+	// different strats : 
+		// Glogab flags :
+		//	-	SCOTCH_STRATDEFAULT
+		//	-	SCOTCH_STRATBALANCE 	-- enforce load balance
+		//	-	SCOTCH_STRATQUALITY 	-- privilege quality over speed
+		//	-	SCOTCH_STRATSAFETY	-- avoid fpe
+		//	-	SCOTCH_STRATSPEED	-- privilege speed over quality
+		// Partitioning flags :
+		//	-	SCOTCH_STRATRECURSIVE	-- use recursive bipartitioning methods and not direct kway methods
+		//	-	SCOTCH_STRATREMAP	-- use the strategy for rzmmaping an existing partition
 	SCOTCH_Graph graph ; 
 	SCOTCH_Num baseval = 0 ; 
 	SCOTCH_Num vertnbr = 6 ; // nombre de sommets
@@ -32,24 +40,22 @@ int main() {
         SCOTCH_stratInit(&strat); //defaut strategy is used
         SCOTCH_graphInit(&graph);
 
-
+	// Fill the source graph
 	SCOTCH_graphBuild(
 			&graph, 
-			baseval,
-			vertnbr,
-			verttab,
-			nullptr,
-			nullptr,
-			nullptr,
-			edgenbr,
-			edgetab,
-			nullptr);
+			baseval,	// graph based value for index arrays
+			vertnbr,	// number of vertices
+			verttab,	// adjacency index array, of size (vertnbr+1)
+			nullptr,	// adjacency end index array, of size (vertnbr) -- optional
+			nullptr,	// vertex load array, of size (vertnbr) -- optional
+			nullptr,	// vertex label array, of size (vertnbr) -- optional
+			edgenbr,	// the number of arcs (twice the number of edges)
+			edgetab,	// adjacency array, of size at least (edgenbr)
+			nullptr);	// arc load array, of size (edgenbr) -- optional
 	SCOTCH_graphCheck(&graph);
 
 	auto parttab = std::vector<SCOTCH_Num>(vertnbr) ; 
 	SCOTCH_graphPart(&graph, partnbr, &strat, parttab.data()) ; 
-
-	// show the partition
 
 	std::cout << "Graph partitioning :" << std::endl;
 	for (SCOTCH_Num i = 0; i < vertnbr; ++i) {
@@ -57,7 +63,9 @@ int main() {
 	}
 
 	SCOTCH_graphExit(&graph) ; 
-	SCOTCH_stratExit(&strat) ; 
+	SCOTCH_stratExit(&strat) ;	// free the content of strat
+
+	return 0 ;
 
 }
 
